1ea183db7ca5df4dfdc26a2551f76a1ef387d571
FrameMuse <minicablestone@gmail.com>
2021-09-22 07:36:04 +0300

    add Handler

---
+++ src/main/java/org/server/Handler.java
@@ -0,0 +1,25 @@
+package org.server;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+public class Handler implements HttpHandler {
+  private int status;
+  private String response;
+
+  public Handler(int status, String response) {
+    this.status = status;
+    this.response = response;
+  }
+
+  @Override
+  public void handle(HttpExchange request) throws IOException {
+    request.sendResponseHeaders(this.status, this.response.length());
+    OutputStream os = request.getResponseBody();
+    os.write(this.response.getBytes());
+    os.close();
+  }
+}
